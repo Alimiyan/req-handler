aa6b7d6bdc7a6ca9674bdeffbe1e450521a1b4e0
alimiyan <alimiyan123@gmail.com>
2022-09-07 22:01:45 +0530

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import "./Home.css";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "../List/List";
+
 function Home() {
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
   return (
-    <div>
+    <div className="main">
       <Navbar />
-      <h1>Hello world</h1>
+      <h1 className="hm">The quickest way to write a request letter</h1>
+      <div className="search">
+        <textarea className="txtar" placeholder="Type your request here"
+        onChange={inputHandler}
+        />
+      </div>
+      <List input={inputText} />
     </div>
   );
 }
